866aa5d785fc7b8a430296770f05be851224e22c
makeme.website <fosterluck777@gmail.com>
2019-03-29 02:56:55 +0200

    add js for render same product page, fix category products display

--- js/category.js
+++ js/category.js
@@ -1,10 +1,13 @@
+var idKey = location.search.slice(1);
 var xhr = new XMLHttpRequest(); // создаем XML функцию
 var products;
 var product = document.createElement('div');
 var productsAll = document.getElementById('products');
+var pdp = document.getElementById('pdp');
 var slideWrapper = document.querySelector('.images'); 
 var categoryLinks = document.querySelector('.links');
 var addProduct = document.getElementById('add');
+var idKey = location.search.slice(1);
 var productItem;
 
 
@@ -39,8 +42,23 @@ xhr.onreadystatechange = function(){
         </a>
         `;
         product.innerHTML += productItem; 
+        if(location.pathname=='/html/category-all.html'){
         productsAll.appendChild(product);
+            } 
         }
         slideWrapper.innerHTML +=`<a class="slide" href="../html/pdp.html?${products[i].id}"><li><img src="${products[i].mainImage}"></li></a>`;
+        
+    }
+    if(location.pathname=='/html/pdp.html'){
+        productItem = `
+    <div>
+    <img src="${products[idKey].mainImage}" width="50%" /> 
+    <h2 class="heading3">${products[idKey].title}</h2> 
+    <h3 class="subheader2">${products[idKey].description}</h3>
+    <h3 class="subheader3">${products[idKey].price} ${products[idKey].currency}</h3> 
+    <button href="" class="btn2 spacing" id="add">Add</button> 
+    </div>
+    `;
+        pdp.innerHTML += productItem;
     }
 }
